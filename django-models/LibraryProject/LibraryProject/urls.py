0d81a16a20a2d9e13aa7484ca84266d6e43a8eac
shammah01 <shammahsemali@gmail.com>
2025-09-19 14:50:50 -0700

    Add function-based and class-based views, URL routing, and templates for relationship_app

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include   # ✅ add include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('relationship/', include('relationship_app.urls')),
 ]
 
