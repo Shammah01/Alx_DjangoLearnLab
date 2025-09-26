591c921ab705a446efdf7ca0a23266c48cccc0a9
shammah01 <shammahsemali@gmail.com>
2025-09-25 17:48:09 -0700

    Added user authentication views, urls, and templates to relationship_app

---
+++ django-models/LibraryProject/relationship_app/templates/relationship_app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+# Register view
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto login after register
+            return redirect("list_books")  # redirect anywhere you like
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
+
+# Login view
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("list_books")
+    else:
+        form = AuthenticationForm()
+    return render(request, "relationship_app/login.html", {"form": form})
+
+# Logout view
+def logout_view(request):
+    logout(request)
+    return render(request, "relationship_app/logout.html")
+
