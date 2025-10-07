23a30b398aa579d446cfa4e93e0a793c5a809de5
shammah01 <shammahsemali@gmail.com>
2025-10-07 12:06:42 -0700

    Updated URL patterns for add_book and edit_book views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,23 +1,8 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-
-    # Role-based views
-    path('admin-view/', views.admin_view, name='admin_view'),
-    path('librarian-view/', views.librarian_view, name='librarian_view'),
-    path('member-view/', views.member_view, name='member_view'),
-]
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('add-book/', views.add_book, name='add_book'),
-    path('edit-book/', views.edit_book, name='edit_book'),
-    path('delete-book/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/', views.edit_book, name='edit_book'),
+    path('delete_book/', views.delete_book, name='delete_book'),
 ]
