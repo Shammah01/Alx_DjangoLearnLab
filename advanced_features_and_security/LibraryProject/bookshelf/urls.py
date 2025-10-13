36bb7cc361e6c043bda79264cffb2ca9f80dc0af
shammah01 <shammahsemali@gmail.com>
2025-10-13 05:04:09 -0700

    Added permission-protected book_list view

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('books/add/', views.add_book, name='add_book'),
     path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
     path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+    path('books/', views.book_list, name='book_list'),
 ]
 
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -56,3 +56,12 @@ def delete_book(request, pk):
 
     return render(request, 'bookshelf/delete_book.html', {'book': book})
 
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
